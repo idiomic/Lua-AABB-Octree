5e9f9721f7f386be71af38da8aca80c123c6f807
Tyler Hoyer <tylerrichardhoyer@gmail.com>
2020-02-22 19:07:05 -0700

    use Roblox Vector3, use keys instead of indexes, more selective intersection and handling of values on edges

--- Octree.lua
+++ Octree.lua
@@ -1,5 +1,5 @@
 --Tyler R. Hoyer
---August 22, 2014
+--February 22, 2020
 --Volumetric AABB Octree with Intersection Test and Commenting
 --TylerRichardHoyer@gmail.com
 
@@ -9,229 +9,135 @@ local Octree_metatable = {__index = Octree}
 --The smallest size of a cell
 local MINIMUM_SIZE = 1
 
---Create a new Octree
---Returns a table
-function Octree.Octree(x, y, z, size) 
-	return setmetatable({
-		x, y, z; --The center (1, 2, 3)
-		
-		size; --The size of the cell as power of 2 (4)
-		
-		{}; --The list of values inside of the cell (5)
-		
-		--The eight sub-cells (xyz sorted, lowest first)
-		false, false, false, false; --(6, 7, 8, 9)
-		false, false, false, false; --(10, 11, 12, 13)
-	}, Octree_metatable)
+local function NIL()
+	return
 end
 
---Get the index of a child
---Returns an integer or false
-function Octree:child(min_x, min_y, min_z, max_x, max_y, max_z)
+local pow2 = {
+	2^0;
+	2^1;
+	2^2;
+}
 
-	--The size of the cell's children
-	local children_size = 2^(self[4] - 1)
-	
-	--Check that the children are big enough
-	if children_size < MINIMUM_SIZE then
-		return false
+local toCoord = {
+	'X';
+	'Y';
+	'Z';
+}
+
+function Octree.new(origin, size)
+	assert(size >= MINIMUM_SIZE, 'Requested size is too small')
+
+	local new
+	if size == MINIMUM_SIZE then
+		new = {
+			origin = origin;
+			size = size;
+			values = {};
+			child = NIL;
+			num_children = 0;
+		}
+	else
+		new = {
+			origin = origin;
+			size = size;
+			values = {};
+			num_children = 0;
+			nil, nil, nil, nil; --(1, 2, 3, 4)
+			nil, nil, nil, nil; --(5, 6, 7, 8)
+		}
 	end
-
-	--The cell's center point
-	local x = self[1]
-	local y = self[2]
-	local z = self[3]
-
-	--The min point's octant relative to the center point of the cell
-	local lesser_x = min_x <= x
-	local lesser_y = min_y <= y
-	local lesser_z = min_z <= z
-	
-	--Check if the max and min points are in the same child
-	if lesser_x == (max_x <= x) 
-		and lesser_y == (max_y <= y) 
-		and lesser_z == (max_z <= z) then
-		
-		--Return the child's index
-		return 6 
-			+ (lesser_x and 0 or 4) 
-			+ (lesser_y and 0 or 2) 
-			+ (lesser_z and 0 or 1)
-	end
-
-	--Return false, in multiple children
-	return false
+	return setmetatable(new, Octree_metatable)
 end
 
---Remove a value
---Returns a boolean
-function Octree:remove(min_x, min_y, min_z, max_x, max_y, max_z, value)
-
-	--If the value is in a child, get it's index
-	local child_index = self:child(min_x, min_y, min_z, max_x, max_y, max_z)
-	
-	--Check if the value belongs in a child
-	if child_index then
-		local child = self[child_index]
-
-		if not child then
-			--Value's container does not exist, return cell not changed
-			return false
-		end
-
-		--Remove the value from the child cell
-		local child_updated = child:remove(min_x, min_y, min_z, max_x, max_y, max_z, value)
-
-		--Check if the child can be deleted
-		--Must have been updated, have no values, and have no children
-		if child_updated and #child[5] == 0 and not (
-			child[6] or child[7] or child[8] or child[9] 
-			or child[10] or child[11] or child[12] or child[13]) then
-			self[child_index] = false
-
-			--Value removed, return cell changed
-			return true
-		end
-
-		--Value removed, return cell not changed
-		return false
-	end
-
-	--Search the cell's values
-	local values = self[5]
-	for i = 1, #values do
-
-		--Check if the value is the value that needs to be deleted
-		if values[i] == value then
-			table.remove(values, i)
-
-			--Value removed, return cell changed
-			return true
-		end
-	end
-	
-	--Value not found, return cell not changed
-	return false
-end
-
---Add a value
---Returns nil
-function Octree:add(min_x, min_y, min_z, max_x, max_y, max_z, value)
-
-	--If the value is in a child, get it's index
-	local child_index = self:child(min_x, min_y, min_z, max_x, max_y, max_z)
-	
-	--Check if the value belongs in a child
-	if child_index then
-		local child = self[child_index]
-
-		--Create the child if it does not exist
-		if not child then
-			local offset = 2 ^ (self[4] - 2)
-			local child_x = self[1] + (min_x <= self[1] and -offset or offset)
-			local child_y = self[2] + (min_y <= self[2] and -offset or offset)
-			local child_z = self[3] + (min_z <= self[3] and -offset or offset)
-			child = Octree.Octree(child_x, child_y, child_z, self[4] - 1)
-			self[child_index] = child
-		end
-		
-		--Add the value to the child (tail call for speed, returns nil)
-		return child:add(min_x, min_y, min_z, max_x, max_y, max_z, value)
-	end
-	
-	--Add the value to this cell
-	local values = self[5]
-	values[#values + 1] = value
-end
-
---Get values intersecting an AABB
---Returns nil, result argument contains resultant values
---Beware: this recursive function calls itself in a sub-recursive function
-function Octree:intersection(min_x, min_y, min_z, max_x, max_y, max_z, result)
-
-	--Append the values in the cell to the result
-	local values = self[5]
-	local numResult = #result
-	for i = 1, #values do
-		result[numResult + i] = values[i]
-	end
-
-	--The center point of the cell
-	local x = self[1]
-	local y = self[2]
-	local z = self[3]
-
-	--The min point's octant relative to the center of the cell
-	local lesser = {
-		min_z <= z,
-		min_y <= y,
-		min_x <= x}
-
-	--The true if the min and max points are in different octants
-	local split = {
-		lesser[1] ~= (max_z <= z),
-		lesser[2] ~= (max_y <= y),
-		lesser[3] ~= (max_x <= x)}
-
-	--This is very complex on multiple levels. The goal is to find out what octants contain
-	--the AABB defined by the min and max points. It does this by checking each axis. For each
-	--axis, the min and max points can be less than, greater than, or split around the center
-	--offset. Once one axis is determined, then it checks the next axis. Once all three are
-	--checked, it checks the current point defined by them. If an axis is split, it checks the
-	--first side for the rest of the axises, then checks the other side. It is simplest, fastest,
-	--and easiest to write in a recursive function.
-	local function check(current, i)
-
-		--If the checks are done
-		if i == 0 then
-			local child = self[current]
-			--Check if the child exists
-
-			if child then
-				--Add values contained in the child
-				return child:intersection(min_x, min_y, min_z, max_x, max_y, max_z, result)
+function Octree:index(min, max)
+	local o = self.origin
+	local i = 1
+	for axis, coord in ipairs(toCoord) do
+		if min[coord] > o[coord] then
+			i = i + pow2[axis]
+		elseif max[coord] > o[coord] then
+			if min[coord] < o[coord] then
+				return
 			end
+			i = i + pow2[axis]
+		end
+	end
+	return i
+end
 
-		--Check if split
-		elseif split[i] then
-			--Add lesser side
-			check(current, i - 1)
-			--Add greater side
-			return check(current + 2^(i - 1), i - 1)
+function Octree:remove(min, max, value)
+	local n = self.num_children
+	local i = self:index(min, max)
 
-		--Check if on lesser side
-		elseif lesser[i] then
-			--Add lesser side
-			return check(current, i - 1)
-
-		else
-			--Add greater side
-			return check(current + 2^(i - 1), i - 1)
+	if not i or self.size <= MINIMUM_SIZE then
+		self.values[value] = nil
+	else
+		if self[i] and self[i]:remove(min, max, value) then
+			n = n - 1
+			self[i] = nil
+			self.num_children = n
 		end
 	end
 
-	--Start the checks. Returns nil
-	return check(6, 3)
+	return n == 0 and next(self.values) == nil
 end
 
---Print structure
---Returns nil
-function Octree:print(tabs)
-	tabs = tabs or ""
-	
-	--Print center
-	print(tabs .. "Center: <" .. self[1] .. ", " .. self[2] .. ", " .. self[3] .. ">")
+function Octree:insert(min, max, value)
+	if self.size <= MINIMUM_SIZE then
+		self.values[value] = true
+		return
+	end
 
-	--Print values
-	print(tabs .. "Values: {" .. table.concat(self[5], ", ") .. "}")
+	local i = self:index(min, max)
+	if not i then
+		self.values[value] = true
+		return
+	end
 
-	--Print children
-	tabs = tabs .. "\t"
-	for i = 6, 13 do
+	local child = self[i]
+	if not child then
+		local origin = self.origin
+		local offset = self.size * 0.5
+		child = Octree.new(
+			Vector3.new(
+				origin.X + (min.X <= origin.X and -offset or offset),
+				origin.Y + (min.Y <= origin.Y and -offset or offset),
+				origin.Z + (min.Z <= origin.Z and -offset or offset)
+			),
+			offset
+		)
+		self.num_children = self.num_children + 1
+		self[i] = child
+	end
 
-		--Check if the child exists and print it
+	return child:insert(min, max, value)
+end
+
+function Octree:_check(min, max, result, i, axis)
+	if axis == 0 then
 		if self[i] then
-			self[i]:print(tabs)
+			return self[i]:intersection(min, max, result)
 		end
+		return
+	end
+	
+	local coord = toCoord[axis]
+	if max[coord] <= self.origin[coord] then
+		return self:_check(min, max, result, i, axis - 1)
+	elseif min[coord] >= self.origin[coord] then
+		return self:_check(min, max, result, i + pow2[axis], axis - 1)
+	else
+		self:_check(min, max, result, i, axis - 1)
+		return self:_check(min, max, result, i + pow2[axis], axis - 1)
 	end
 end
+
+function Octree:intersection(min, max, result)
+	for value in pairs(self.values) do
+		result[value] = true
+	end
+	return self:_check(min, max, result, 1, 3)
+end
+
+return Octree
